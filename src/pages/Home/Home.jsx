68ea67079b26c8de72a5270a30325cc6200adf12
bigmanabel <bigmanabel@outlook.com>
2024-08-13 11:18:04 +0000

    1400px responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,11 +6,11 @@ import SideNav from "../../components/SideNav/SideNav";
 const Home = () => {
   return (
     <div className="flex h-screen sm:flex-col lg:flex-row">
-      <div className="lg:w-1/5 min-[1400px]:col-span-2 h-full">
+      <div className="lg:w-[25%] xl:w-1/5 min-[1400px]:w-1/5 h-full">
         <SideNav />
       </div>
 
-      <div className="lg:w-4/5 min-[1400px]:col-span-10 bg-[#F8FAFB] flex flex-col">
+      <div className="lg:w-[75%] xl:w-4/5 min-[1400px]:w-4/5 bg-[#F8FAFB] flex flex-col">
         <main className="overflow-y-auto">
           <Header />
           <Main />
